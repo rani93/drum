8e758aff2ae70d16ae80ec1b5ceecc2509ff325d
rani93 <114481571+rani93@users.noreply.github.com>
2023-06-15 19:03:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,86 @@
+// document.querySelectors("button")[i].addEventListener("click",handleClick);
+// function handleClick(){
+//    alert("hey hello");
+// }
+// var a= (document.querySelectorAll("button")).length;
+
+
+// for(var i=0;i<a;i++){
+//     document.querySelectorAll("button")[i].addEventListener("click", function (){
+//         alert("hey hello");
+
+//     });
+// }
+//here we have to change the query selector catagory because if any other button is presnt then it will consider that button also which we dont required so we have to change it
+var a= (document.querySelectorAll(".drum")).length;
+
+
+for(var i=0;i<a;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+       var buttonInnerHTML= this.innerHTML;
+       makeSound(buttonInnerHTML);
+       buttonAnimation(buttonInnerHTML);
+
+    });
+}
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+    
+   });
+   //above function is use when we press key  from the keyboard aurr listen when we press the key the system will resond;
+
+function makeSound(key){
+    switch (key){
+        case "w":{
+            var audio = new Audio("sounds/crash.mp3"); 
+            audio.play();
+            break;
+        }
+        case "a":{
+            var audio = new Audio("sounds/kick-bass.mp3"); 
+            audio.play();
+            break;
+        }
+        case "s":{
+            var audio = new Audio("sounds/snare.mp3"); 
+            audio.play();
+            break;
+        }
+        case "d":{
+            var audio = new Audio("sounds/tom-1.mp3"); 
+            audio.play();
+            break;
+        }
+        case "j":{
+            var audio = new Audio("sounds/tom-2.mp3"); 
+            audio.play();
+            break;
+        }
+        case "k":{
+            var audio = new Audio("sounds/tom-3.mp3"); 
+            audio.play();
+            break;
+        }
+        case "l":{
+            var audio = new Audio("sounds/kick-bass.mp3"); 
+            audio.play();
+            break;
+        }
+       } 
+
+
+}
+function buttonAnimation(currentKey){
+    var activeButton=document.querySelector("."+currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+      }, 100);
+   
+}
+
+
+
+
+ 
